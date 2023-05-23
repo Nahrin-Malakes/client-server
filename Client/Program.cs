9b9704a480ea512ed521cba8b91186ca65510c35
NahrinMalakes <nahrinmalakes12@gmail.com>
2023-05-23 09:35:05 +0300

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,202 @@
+﻿// DavidCli (4 may , 23)
+using System; //EventHandler
+using System.Windows.Forms; //Form
+using System.Drawing; //Point
+using System.Text; //Encoding
+using System.IO; //TextReader
+using System.Drawing.Drawing2D; //GraphicsPath
+using System.Net; //IPAddress, IPEndpoint
+using System.Net.Sockets; //UdpClient
+using System.Threading; //Sleep
+
+//class CircularButton
+class CircularButton : Button
+{
+    protected override void OnPaint(PaintEventArgs pevent)
+    {
+        GraphicsPath gp = new GraphicsPath();
+        gp.AddEllipse(0, 0, ClientSize.Width, ClientSize.Height);
+        this.Region = new Region(gp);
+        base.OnPaint(pevent);
+    }
+}
+
+class MyForm : Form
+{
+    private CircularButton bt2;
+    private Label lb, lb1;
+    private TextBox tbox;
+
+    private bool stopReceive;
+    private Thread rec;
+    private UdpClient udp;
+
+    private MainMenu menu;
+
+    private int CLIENT_PORT;
+    private int SERVER_PORT;
+    private string SERVER_IP;
+
+    public MyForm()
+    {
+        //ports and server ip
+        CLIENT_PORT = 15000;
+        SERVER_PORT = 15000;
+        SERVER_IP = "192.168.1.104";
+
+        //UdpClient
+        rec = null;
+        udp = new UdpClient(CLIENT_PORT); //15000 - client port (to receive a message from a server)
+        stopReceive = false;
+
+        //form
+        StartPosition = FormStartPosition.Manual;
+        Top = 30;
+        Left = 1275;
+        ClientSize = new Size(420, 300); //form size
+        BackColor = Color.FromArgb(200, 218, 185);//form color
+        FormBorderStyle = FormBorderStyle.Fixed3D;
+        Text = "משתמש של דוד";
+        ControlBox = false;
+        MinimizeBox = false;
+        MaximizeBox = false;
+
+        //button2
+        bt2 = new CircularButton();
+        bt2.Size = new Size(120, 70);
+        bt2.Location = new Point(160, 190);
+        bt2.Font = new Font("Ariel", 15F, FontStyle.Bold, GraphicsUnit.Point, (byte)0);
+        bt2.Text = "שלח";
+        bt2.BackColor = Color.Black;
+        bt2.ForeColor = Color.White;
+        bt2.FlatStyle = FlatStyle.Flat;
+        bt2.FlatAppearance.BorderColor = Color.Black;
+        bt2.FlatAppearance.BorderSize = 0;
+        bt2.Visible = false;
+        Controls.Add(bt2);
+        bt2.Click += new EventHandler(bt2_Click);
+
+        //label
+        lb = new Label();
+        lb.AutoSize = true;
+        lb.Font = new Font("Ariel", 35F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, (byte)0);
+        lb.Location = new Point(80, 45); //location label
+        lb.Text = "תתחיל לעבוד";
+        lb.ForeColor = Color.Olive;
+        Controls.Add(lb);
+
+        //label1
+        lb1 = new Label();
+        lb1.AutoSize = true;
+        lb1.Font = new Font("Ariel", 10F, FontStyle.Bold, GraphicsUnit.Point, (byte)0);
+        lb1.Location = new Point(5, 135); //location label
+        lb1.Text = "שם של הקובץ";
+        lb1.ForeColor = Color.Black;
+        Controls.Add(lb1);
+        
+        //textbox 
+        tbox = new TextBox();
+        tbox.Location = new Point(100, 130); //textbox location
+        tbox.TabIndex = 0;
+        tbox.ClientSize = new Size(250, 1);
+        tbox.BackColor = SystemColors.GradientActiveCaption;
+        tbox.ForeColor = Color.Black; //font color
+        Font myfont = new Font("Arial", 14.0f); //font size
+        tbox.Font = myfont;
+        tbox.RightToLeft = RightToLeft.No;
+        tbox.BorderStyle = BorderStyle.FixedSingle;
+        tbox.Text = "";
+        Controls.Add(tbox);
+
+        //menu
+        this.menu = new MainMenu();
+        MenuItem m1 = new MenuItem("להתחיל");
+        this.menu.MenuItems.Add(m1);
+        MenuItem i1 = new MenuItem("תתחיל");
+        m1.MenuItems.Add(i1);
+        MenuItem i2 = new MenuItem("תעצור");
+        m1.MenuItems.Add(i2);
+        i1.Click += Start;
+        i2.Click += Stop;
+        Menu = menu;
+    }
+
+    //5. button 2 event (שלח)
+    private void bt2_Click(object sender, EventArgs e)
+    {
+        if (tbox.Text.Trim().Equals("sort"))
+        {
+            udpSend(SERVER_IP, SERVER_PORT, "sort");
+            tbox.Text = "";
+        }
+        else
+        {
+            TextReader t = new StreamReader("D:\\LIM\\DavidSerCli\\" + tbox.Text.Trim() + ".txt");
+            string mymessage = t.ReadToEnd();
+            t.Close();
+            udpSend(SERVER_IP, SERVER_PORT, mymessage);
+            tbox.Text = "";
+        }            
+    }
+
+    //6. udpSend
+    private int udpSend(string ipAddress, int port, string myMessage)
+    {
+        byte[] myMessageBytes = Encoding.UTF8.GetBytes(myMessage);
+        UdpClient udp = new UdpClient();
+        IPAddress ipad = IPAddress.Parse(ipAddress);
+        IPEndPoint ipen = new IPEndPoint(ipad, port);
+        int send = udp.Send(myMessageBytes, myMessageBytes.Length, ipen);
+        udp.Close();
+        return send;
+    }
+
+    //7. ReceiveMessage
+    void ReceiveMessage()
+    {
+        try
+        {
+            while (true)
+            {
+                IPEndPoint ipendpoin = null;
+                byte[] messageFromServerByte = udp.Receive(ref ipendpoin);
+                string messageFromServerStr = Encoding.UTF8.GetString(messageFromServerByte);
+                MessageBox.Show(messageFromServerStr);
+                if (stopReceive == true) break;
+            }
+        }
+        catch (Exception e)
+        {
+            MessageBox.Show(e.Message);
+        }
+    }
+
+    //8. Start 
+    private void Start(object sender, EventArgs e)
+    {
+        stopReceive = false;
+        rec = new Thread(new ThreadStart(ReceiveMessage));
+        rec.Start();
+        lb.Text = "אני כבר מתחיל";
+        lb.Location = new Point(65, 45); //location label
+        bt2.Visible = true;
+    }
+
+    //9. Stop
+    private void Stop(object sender, EventArgs e)
+    {
+        stopReceive = true;
+        if (udp != null)
+            udp.Close();
+        if (rec != null)
+            rec.Join();
+        Close();
+    }
+
+    //10. main
+    [STAThread]
+    static void Main()
+    {
+        Application.Run(new MyForm());
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,263 @@
+﻿/*
+    DavidSer; 4  may, 23
+ * 
+ * */
+using System; //Exception, Environment, EventHandler
+using System.Windows.Forms; //Label, MessageBox
+using System.Drawing; //Color, Font Size, Point
+using System.IO; //File
+using System.Text; //Encoding
+using System.Diagnostics; //Process
+using System.Threading; //Thread
+using System.Net.Sockets; //UdpClient
+using System.Net; //IPEndPoint, udp
+using System.Drawing.Imaging; //Image
+
+//class Queue
+class Queue<T>
+{
+    private T[] x;
+    private int n, p;
+
+    //Stack
+    public Queue()
+    {
+        n = 0; //makom avur ereh haba
+        p = 0; //rosh hator
+        x = new T[100];
+    }
+
+    //Head
+    public T Head()
+    {
+        return x[p];
+    }
+
+    //Remove
+    public T Remove()
+    {
+        p++;
+        return x[p - 1];
+    }
+
+    //Insert
+    public void Insert(T k)
+    {
+        x[n++] = k;
+    }
+
+    //IsEmpty
+    public bool IsEmpty()
+    {
+        if (n == p)
+            return true;
+        else
+            return false;
+    }
+
+    //ToString
+    public override string ToString()// After: q.Insert(1); q.Insert(2); q.Insert(3); we have: ---> <3 2 1> --->
+    {
+        string s = "< ";
+        for (int i = n - 1; i >= p; i--)
+        {
+            s += x[i] + " ";
+        }
+        return s + ">";
+    }
+}
+
+//класс MyForm
+class MyForm : Form
+{
+    //кнопки и лейбл
+    private Button btn1, btn2;
+    private Label lb;
+    private int N;
+    private Queue<double> q;
+
+    //сокет
+    private Thread rec;
+    private UdpClient udp;
+    private bool stopReceive;
+
+    //1. конструктор
+    public MyForm()
+    {
+        N = 0;
+        q = new Queue<double>();
+
+        //сокет
+        rec = null;
+        udp = new UdpClient(15000); //15000 - порт сервера
+        stopReceive = false;
+
+        //форма
+        ClientSize = new System.Drawing.Size(360, 400); //size of my form
+        Text = "שרת של דוד";
+        ControlBox = true; //false - remove all the Control Buttons (e.g. Minimize, Maximize, Exit) and also the icon
+        StartPosition = FormStartPosition.Manual;
+        BackColor = Color.Azure;
+        ShowInTaskbar = false;
+        Left = 1305; //координаты формы
+        Top = 30; //координаты формы
+        ControlBox = false;
+        MinimizeBox = false;
+        MaximizeBox = false;
+
+        //кнопка 1
+        btn1 = new Button();
+        btn1.Enabled = true;
+        btn1.Text = "Start";
+        btn1.Font = new Font("Arial", 15.5F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+        btn1.Size = new Size(70, 70);
+        btn1.FlatAppearance.BorderSize = 0;
+        btn1.FlatStyle = FlatStyle.Flat;
+        btn1.BackColor = Color.Blue;
+        btn1.FlatAppearance.BorderColor = Color.Blue; //border color
+        btn1.ForeColor = Color.White;
+        btn1.Location = new Point(150, 120); //coordinates of my button
+        Controls.Add(btn1);
+        btn1.Click += new EventHandler(btn1_Click);
+
+        //кнопка 2
+        btn2 = new Button();
+        btn2.Enabled = true;
+        btn2.Text = "Stop";
+        btn2.Font = new Font("Arial", 15.5F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+        btn2.Size = new Size(70, 70);
+        btn2.FlatAppearance.BorderSize = 0;
+        btn2.FlatStyle = FlatStyle.Flat;
+        btn2.BackColor = Color.Black;
+        btn2.FlatAppearance.BorderColor = Color.Black; //border color
+        btn2.ForeColor = Color.White;
+        btn2.Location = new Point(150, 250); //coordinates of my button
+        Controls.Add(btn2);
+        btn2.Click += new EventHandler(btn2_Click);
+
+        //лейбл
+        lb = new Label();
+        lb.AutoSize = true;
+        lb.Font = new System.Drawing.Font("Guttman-Aharoni", 30.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, (byte)0);
+        lb.Location = new System.Drawing.Point(70, 30); //coordinates of the label
+        lb.Size = new System.Drawing.Size(352, 24);
+        lb.Text = "השרת שלי";
+        lb.ForeColor = Color.Black;
+        Controls.Add(lb);
+    }
+
+    //2. обработчик первой кнопки Start
+    private void btn1_Click(object sender, EventArgs e)
+    {
+        stopReceive = false;
+        rec = new Thread(new ThreadStart(ReceiveMessage));
+        rec.Start();
+        lb.Text = "    השרת מאזין";
+        lb.ForeColor = Color.Red;
+        btn1.BackColor = Color.Green;
+        btn1.FlatAppearance.BorderColor = Color.Green; //border color
+        btn2.BackColor = Color.Goldenrod;
+        btn2.FlatAppearance.BorderColor = Color.Goldenrod; //border color
+        btn1.Visible = false;
+        btn2.Location = new Point(150, 180); //coordinates of my button
+        lb.Location = new System.Drawing.Point(11, 70);
+    }
+
+    //3. обработчик второй кнопки Stop
+    private void btn2_Click(object sender, EventArgs e)
+    {
+        stopReceive = true;
+        if (udp != null) udp.Close();
+        if (rec != null) rec.Join();
+        lb.Text = "Server";
+        lb.Location = new System.Drawing.Point(110, 30);
+        Close();
+    }
+
+    //4. функция ReceiveMessage для получения данныx от клиента
+    void ReceiveMessage()
+    {
+        try
+        {
+            while (true)
+            {
+                IPEndPoint ipendpoint = null;
+                byte[] messageByte = udp.Receive(ref ipendpoint);//ipendpoint=192.168.1.186:64000, где 64000 - порта клиента               
+                string messageString = Encoding.UTF8.GetString(messageByte);
+
+                if (messageString.Equals("sort"))
+                {
+                    //create arr[]
+                    double[] arr = new double[N];
+                    int j = 0;                    
+                    while (!q.IsEmpty())
+                    {
+                        double x = q.Remove();
+                        arr[j++] = x;
+                    }
+
+                    //sort arr[]
+                    BubbleSort(arr);
+
+                    //create list to send
+                    string mehirim = "Ordered prices:\n\n";
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        mehirim += arr[i] + "\n";
+                    }
+                    udpSend(ipendpoint.Address.ToString(), 15000, mehirim);//где ipendpoint.Address.ToString() = 192.168.1.186 - ip клиента
+                }
+                else
+                {
+                    int x = messageString.IndexOf("Price:");
+                    string myPriceTemp = messageString.Substring(x + 6);
+                    int y = myPriceTemp.IndexOf("+");
+                    string myPrice = myPriceTemp.Substring(0, y);
+                    MessageBox.Show((N + 1) + ") " + myPrice + "");
+                    q.Insert(double.Parse(myPrice));
+                    N++;
+                }
+
+                if (stopReceive == true) break;
+            }
+        }
+        catch (Exception e)
+        {
+            MessageBox.Show(e.Message);
+        }
+    }
+
+    private int udpSend(string ipAddress, int port, string myMessage)
+    {
+        byte[] myMessageBytes = Encoding.UTF8.GetBytes(myMessage);
+        UdpClient udp = new UdpClient();
+        IPAddress ipad = IPAddress.Parse(ipAddress);
+        IPEndPoint ipen = new IPEndPoint(ipad, port);
+        int send = udp.Send(myMessageBytes, myMessageBytes.Length, ipen);
+        udp.Close();
+        return send;
+    }
+
+    public static void BubbleSort(double[] a)
+    {
+        for (int j = 0; j < a.Length - 1; j++)
+        {
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+                if (a[i] > a[i + 1])
+                {
+                    double t = a[i];
+                    a[i] = a[i + 1];
+                    a[i + 1] = t;
+                }
+            }
+        }
+    }
+
+    //5. Главный модуль 
+    [STAThread]
+    static void Main()
+    {
+        Application.Run(new MyForm());
+    }
+}
